449f5fd4b98f89ee8e0983ca5bf07fd3de1923d0
Kshitij1809 <kshitij48@gmail.com>
2024-12-22 09:07:34 +0530

    program add for 1st time

---
+++ Program.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter third number: ");
+        double num3 = scanner.nextDouble();
+
+        double sum = num1 + num2 + num3;
+
+        System.out.println("The sum of " + num1 + ", " + num2 + ", and " + num3 + " is: " + sum);
+    }
+}
